e59c20439129e74fba26f9bb1d6a56f895dd3e23
= <rohansinglawork@gmail.com>
2025-06-19 14:17:55 +0530

    privy integration

---
+++ frontend/config/wagmiConfig.ts
@@ -0,0 +1,13 @@
+import {mainnet, sepolia} from 'viem/chains';
+import {http} from 'wagmi';
+
+import {createConfig} from '@privy-io/wagmi';
+
+
+export const config = createConfig({
+  chains: [mainnet, sepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+});
